46be5157b4fffd28d950f450329eeca9e0d9d938
Billy.Zheng <zw963@163.com>
2017-07-23 10:51:39 +0800

    rename application.js to application.rb, Add test example.

--- app/assets/javascripts/application.rb
+++ app/assets/javascripts/application.rb
@@ -3,3 +3,14 @@ require 'clearwater'
 require 'rails-ujs'
 require 'turbolinks'
 require_tree '.'
+
+class Layout
+  include Clearwater::Component
+
+  def render
+    h1('Hello, world!')
+  end
+end
+
+app = Clearwater::Application.new(component: Layout.new)
+app.call
